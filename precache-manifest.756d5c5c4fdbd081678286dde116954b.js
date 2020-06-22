8730249a8cf5df87de6871c4885f773a2682f340
root <root@localhost.localdomain>
2020-06-22 13:58:46 +0545

    Updates

---
+++ precache-manifest.756d5c5c4fdbd081678286dde116954b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56a57aabf6231f345c4b3abef107ce8",
+    "url": "/smart-health-app/index.html"
+  },
+  {
+    "revision": "8ba5a47d0a9ccca5bfca",
+    "url": "/smart-health-app/static/css/main.39271b64.chunk.css"
+  },
+  {
+    "revision": "d4161c87b9c6e9a152ee",
+    "url": "/smart-health-app/static/js/2.3683dc79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-health-app/static/js/2.3683dc79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ba5a47d0a9ccca5bfca",
+    "url": "/smart-health-app/static/js/main.12c33d9e.chunk.js"
+  },
+  {
+    "revision": "97796a7e5a868c515a34",
+    "url": "/smart-health-app/static/js/runtime-main.e3e057ad.js"
+  }
+]);
